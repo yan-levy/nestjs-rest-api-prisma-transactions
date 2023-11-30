990e45e4241fefdd0061caca01fed9bc793490aa
yan-levy <yanbatistacel@gmail.com>
2023-11-30 15:05:12 -0400

    🙈 Transactions using prisma

---
+++ src/not-found-error/not-found-error.filter.ts
@@ -0,0 +1,15 @@
+import { NotFoundError } from './../errors';
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+
+@Catch(NotFoundError)
+export class NotFoundErrorFilter implements ExceptionFilter {
+  catch(exception: NotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    response.status(404).json({
+      statusCode: 404,
+      message: exception.message,
+    });
+  }
+}
